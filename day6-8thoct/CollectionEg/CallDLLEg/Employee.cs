91d561422ec606a92b2e4fe08fe1006b43c9c6a1
SriBhagya <73333359+SriBhagya@users.noreply.github.com>
2020-11-02 13:41:37 +0530

    Add files via upload

---
+++ day6-8thoct/CollectionEg/CallDLLEg/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NormalCalculator;
+
+namespace CallDLLEg
+{
+	class Employee
+	{
+		static void Main()
+		{
+			Calculator calculator = new Calculator();
+			Console.WriteLine("addition of two numbers:{0}", calculator.Addition(10,13));
+			Console.ReadLine();
+		}
+	}
+}
